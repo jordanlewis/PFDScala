ec51808dbef2c21e19b0239ba800eab9d4890637
Jordan Lewis <jordanthelewis@gmail.com>
2012-01-31 02:11:00 -0500

    Typo in exception: head->tail

--- src/main/scala/CustomStack.scala
+++ src/main/scala/CustomStack.scala
@@ -5,7 +5,7 @@ case object EmptyCustomStack extends CustomStack[Nothing] {
   def isEmpty = true
   def cons[T](x: T) = new NonEmptyCustomStack[T](x, EmptyCustomStack)
   def head = throw new IllegalArgumentException("Can't take head of an empty list")
-  def tail = throw new IllegalArgumentException("Can't take head of an empty list")
+  def tail = throw new IllegalArgumentException("Can't take tail of an empty list")
 }
 final case class NonEmptyCustomStack[+T](head: T, tail: CustomStack[T]) extends CustomStack[T] {
   def isEmpty = false

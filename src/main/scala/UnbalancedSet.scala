d7db8e780e5b9866bf39cf8dd13e0b226ca17f02
Jordan Lewis <jordanthelewis@gmail.com>
2012-02-05 12:47:50 -0500

    Add UnbalancedTreeSet implementation

--- src/main/scala/UnbalancedSet.scala
+++ src/main/scala/UnbalancedSet.scala
@@ -20,31 +20,32 @@ final case class NonEmptyUnbalancedSet[T <% Ordered[T]]
     if      (x < value) NonEmptyUnbalancedSet(left insert x, value, right)
     else if (x > value) NonEmptyUnbalancedSet(left, value, right insert x)
     else this
-  override def member(x: T) =
+  def member(x: T) =
     if      (x < value) left member x
     else if (x > value) right member x
     else true
 }
 
 
-//private sealed abstract class Tree[+T]
-//private case object TreeLeaf extends Tree[Nothing]
-//private final case class TreeNode[T](left: Tree[T], value: T, right: Tree[T]) extends Tree[T]
-//
-//class UnbalancedSet[T <% Ordered[T]] private (private val t: Tree[T]) extends Set[T] {
-//  def insert[T](x: T): Set[T] = t match {
-//    case TreeLeaf => new TreeNode(new TreeLeaf[T], x, new TreeLeaf[T])
-//    case tn @ TreeNode(a, y, b) =>
-//      if (x < y) new TreeNode(a.insert(x), y, b)
-//      else if (y < x) new TreeNode(a, y, b.insert(x))
-//      else tn
-//    }
-//  def member[T](x: T): Boolean = (x, t) match {
-//    case (x, TreeLeaf) => false
-//    case (x, TreeNode(a, y, b)) =>
-//      if (x < y) a.member(x)
-//      else if (y < x) b.member(x)
-//      else true
-//    }
-//}
-//
+sealed abstract class Tree[+T]
+case object TreeLeaf extends Tree[Nothing]
+final case class TreeNode[T](left: Tree[T], value: T, right: Tree[T]) extends Tree[T]
+
+class UnbalancedTreeSet[T <% Ordered[T]] private (val t: Tree[T]) extends Set[T] {
+  type t
+  def apply() = TreeLeaf
+  def insert(x: T): UnbalancedTreeSet[T] = t match {
+    case TreeLeaf => new UnbalancedTreeSet(TreeNode(TreeLeaf, x, TreeLeaf))
+    case tn @ TreeNode(a, y, b) =>
+      if (x < y) new UnbalancedTreeSet(TreeNode(new UnbalancedTreeSet(a).insert(x).t, y, b))
+      else if (y < x) new UnbalancedTreeSet(TreeNode(a, y, new UnbalancedTreeSet(b).insert(x).t))
+      else new UnbalancedTreeSet(tn)
+    }
+  def member(x: T): Boolean = (x, t) match {
+    case (x, TreeLeaf) => false
+    case (x, TreeNode(a, y, b)) =>
+      if (x < y) new UnbalancedTreeSet(a).member(x)
+      else if (y < x) new UnbalancedTreeSet(b).member(x)
+      else true
+    }
+}

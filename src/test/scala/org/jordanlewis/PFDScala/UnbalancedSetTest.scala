538cec0f9a00e5eca8dbb927e93c9659de3f79ef
Jordan Lewis <jordanthelewis@gmail.com>
2012-02-05 12:48:16 -0500

    Add a ScalaTest test

---
+++ src/test/scala/org/jordanlewis/PFDScala/UnbalancedSetTest.scala
@@ -0,0 +1,20 @@
+package org.jordanlewis.PFDScala
+
+import org.scalatest.matchers.ShouldMatchers
+import org.jordanlewis.pfds.UnbalancedSet
+import org.scalatest.Spec
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class UnbalancedSetTest extends Spec with ShouldMatchers {
+  describe("An unbalanced set") {
+    it("should contain elements that have been inserted") {
+      val set = UnbalancedSet[Int].insert(3).insert(5).insert(2)
+      set.member(3) should be (true)
+      set.member(2) should be (true)
+      set.member(5) should be (true)
+      set.member(7) should be (false)
+    }
+  }
+}

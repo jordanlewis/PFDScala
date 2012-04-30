55bf651a4303afa2ab05536165572ee2865a51bd
Jordan Lewis <jordanthelewis@gmail.com>
2012-04-30 17:37:26 -0400

    Implement Set with an UnbalancedTreeSet wrapper
    The wrapper behaves like a fully-fledged Set, but underneath the hood it
    just wraps a Tree, using the operations defined on the UnbalancedTreeSet
    object.
    
    I think this does a good job of being idiomatic to Scala, while also
    retaining the semantics and "feel" of the PFDS implementation.
    
    Also remove the TreeSet trait from Tree.scala, making it just a datatype
    set of a classes.

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -3,9 +3,3 @@ package org.jordanlewis.pfds
 sealed abstract class Tree[+T]
 case object TreeLeaf extends Tree[Nothing]
 final case class TreeNode[T](left: Tree[T], value: T, right: Tree[T]) extends Tree[T]
-
-trait TreeSet {
-  def insert[T <% Ordered[T]](x: T, t: Tree[T]): Tree[T]
-  def member[T <% Ordered[T]](x: T, t: Tree[T]): Boolean
-}
-
--- src/main/scala/UnbalancedTreeSet.scala
+++ src/main/scala/UnbalancedTreeSet.scala
@@ -1,20 +1,30 @@
 package org.jordanlewis.pfds
 
-object UnbalancedTreeSet extends TreeSet {
-  def apply() = TreeLeaf
-  def insert[T <% Ordered[T]](x: T, t: Tree[T]): Tree[T] = t match {
+class UnbalancedTreeSet[T] private (tree: Tree[T]) (implicit val ordering: Ordering[T])
+  extends Set[T] {
+
+  def this()(implicit ordering : Ordering[T]) = this(TreeLeaf)(ordering)
+  private def newSet(tree: Tree[T]) = new UnbalancedTreeSet[T](tree);
+
+  def insert(x: T) = newSet(UnbalancedTreeSet.insert(x, tree))
+  def member(x: T) = UnbalancedTreeSet.member(x, tree)
+}
+
+object UnbalancedTreeSet {
+  private def insert[T](x: T, t: Tree[T])(implicit ordering: Ordering[T]): Tree[T] = t match {
     case TreeLeaf => TreeNode(TreeLeaf, x, TreeLeaf)
     case tn @ TreeNode(a, y, b) =>
-      if (x < y) TreeNode(insert(x, a), y, b)
-      else if (y < x) TreeNode(a, y, insert(x, b))
+      if (ordering.lt(x, y)) TreeNode(insert(x, a), y, b)
+      else if (ordering.lt(y, x)) TreeNode(a, y, insert(x, b))
       else tn
   }
-  def member[T <% Ordered[T]](x: T, t: Tree[T]): Boolean = (x, t) match {
-    case (x, TreeLeaf) => false
-    case (x, TreeNode(a, y, b)) =>
-      if (x < y) member(x, a)
-      else if (y < x) member(x, b)
+  private def member[T](x: T, t: Tree[T])(implicit ordering: Ordering[T]): Boolean = (x, t) match {
+    case (_, TreeLeaf) => false
+    case (_, TreeNode(a, y, b)) =>
+      if (ordering.lt(x, y)) member(x, a)
+      else if (ordering.lt(y, x)) member(x, b)
       else true
   }
 }
+
 

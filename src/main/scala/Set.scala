4c86fe2faa57d08f4f910b2e344b2f62cd6684c6
Jordan Lewis <jordanthelewis@gmail.com>
2012-04-30 17:34:27 -0400

    Make Set type-invariant, as it should be

--- src/main/scala/Set.scala
+++ src/main/scala/Set.scala
@@ -1,6 +1,6 @@
 package org.jordanlewis.pfds
 
-trait Set[+T] {
-  def insert[U >: T](x: U): Set[U]
-  def member[U >: T](x: U): Boolean
+trait Set[T] {
+  def insert(x: T): Set[T]
+  def member(x: T): Boolean
 }

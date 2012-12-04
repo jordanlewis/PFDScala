694514da34caf628e1e67a655a72e650e4f931d5
Jordan Lewis <jordanthelewis@gmail.com>
2012-12-04 18:53:26 -0500

    Add size method to BinaryTree

--- src/main/scala/BinaryTree.scala
+++ src/main/scala/BinaryTree.scala
@@ -1,5 +1,11 @@
 package org.jordanlewis.pfds
 
-sealed abstract class BinaryTree[+T]
-case object BinaryTreeLeaf extends BinaryTree[Nothing]
-final case class BinaryTreeNode[T](left: BinaryTree[T], value: T, right: BinaryTree[T]) extends BinaryTree[T]
+sealed abstract class BinaryTree[+T] {
+  def size(): Int
+}
+case object BinaryTreeLeaf extends BinaryTree[Nothing] {
+  def size() = 0
+}
+final case class BinaryTreeNode[T](left: BinaryTree[T], value: T, right: BinaryTree[T]) extends BinaryTree[T] {
+  def size() = 1 + left.size + right.size
+}

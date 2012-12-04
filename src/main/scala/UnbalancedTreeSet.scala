82495cc896b6527ad745ddbb96f57de20f1829dc
Jordan Lewis <jordanthelewis@gmail.com>
2012-12-04 18:54:40 -0500

    Implement exercises 2.{2,3,4}
    Updates UnbalancedTreeSet to be more efficient in insert and member.

--- src/main/scala/UnbalancedTreeSet.scala
+++ src/main/scala/UnbalancedTreeSet.scala
@@ -1,30 +1,36 @@
 package org.jordanlewis.pfds
 
+class ElementAlreadyExistsException extends RuntimeException
+
 class UnbalancedTreeSet[T] private (tree: BinaryTree[T]) (implicit val ordering: Ordering[T])
   extends Set[T] {
 
   def this()(implicit ordering : Ordering[T]) = this(BinaryTreeLeaf)(ordering)
-  private def newSet(tree: BinaryTree[T]) = new UnbalancedTreeSet[T](tree);
+  def newSet(tree: BinaryTree[T]) = new UnbalancedTreeSet[T](tree);
 
-  def insert(x: T) = newSet(UnbalancedTreeSet.insert(x, tree))
-  def member(x: T) = UnbalancedTreeSet.member(x, tree)
+  def insert(x: T) = try {
+    newSet(UnbalancedTreeSet.insert(x, tree, None))
+  } catch {
+    case _:ElementAlreadyExistsException => this
+  }
+  def member(x: T) = UnbalancedTreeSet.member(x, tree, None)
 }
 
 object UnbalancedTreeSet {
-  private def insert[T](x: T, t: BinaryTree[T])(implicit ordering: Ordering[T]): BinaryTree[T] = t match {
-    case BinaryTreeLeaf => BinaryTreeNode(BinaryTreeLeaf, x, BinaryTreeLeaf)
-    case tn @ BinaryTreeNode(a, y, b) =>
-      if (ordering.lt(x, y)) BinaryTreeNode(insert(x, a), y, b)
-      else if (ordering.lt(y, x)) BinaryTreeNode(a, y, insert(x, b))
-      else tn
+  private def insert[T](x: T, t: BinaryTree[T], c: Option[T])(implicit ordering: Ordering[T]): BinaryTree[T] = (t, c) match {
+    case (BinaryTreeLeaf, None) => BinaryTreeNode(BinaryTreeLeaf, x, BinaryTreeLeaf)
+    case (BinaryTreeLeaf, Some(d)) =>
+      if (ordering.equiv(x, d)) throw new ElementAlreadyExistsException
+      else BinaryTreeNode(BinaryTreeLeaf, x, BinaryTreeLeaf)
+    case (BinaryTreeNode(a, y, b), _) =>
+      if (ordering.lt(x, y)) BinaryTreeNode(insert(x, a, c), y, b)
+      else BinaryTreeNode(a, y, insert(x, b, Some(y)))
   }
-  private def member[T](x: T, t: BinaryTree[T])(implicit ordering: Ordering[T]): Boolean = (x, t) match {
-    case (_, BinaryTreeLeaf) => false
-    case (_, BinaryTreeNode(a, y, b)) =>
-      if (ordering.lt(x, y)) member(x, a)
-      else if (ordering.lt(y, x)) member(x, b)
-      else true
+  private def member[T](x: T, t: BinaryTree[T], c: Option[T])(implicit ordering: Ordering[T]): Boolean = (t, c) match {
+    case (BinaryTreeLeaf, None) => false
+    case (BinaryTreeLeaf, Some(d)) => ordering.equiv(x, d)
+    case (BinaryTreeNode(a, y, b), _) =>
+      if (ordering.lt(x, y)) member(x, a, c)
+      else member(x, b, Some(y))
   }
 }
-
-
